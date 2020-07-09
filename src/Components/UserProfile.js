a5c1132b23ab3108d013374f5a4d18505bd2f54a
Oscar Karlsson <oscar.karl00@hotmail.com>
2020-07-09 20:42:59 +0200

    css...

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -7,15 +7,14 @@ const UserProfile = (props) => {
     const Auth = useContext(AuthenticationContext);
 
     return (
-        <div className="user-profile floating-card">
-            <div className="user-profile-content user-profile-content--left">
-                <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
-            </div>
-            <div className="User-profile-content user-profile-content--right">
-                <label>{Auth.UserData.Firstname} {Auth.UserData.Lastname}</label>
-                <label>Email: {Auth.UserData.Email}</label>
-                <label>Username: {Auth.UserData.Username}</label>
-                {props.showChangeImageLink === true ? (<label id="link">Change image</label>): (null)}
+        <div>
+            <h5 style={{ margin: "0 0 7px 0", color: "#707070" }}>You are currently logged in as:</h5>
+            <div className="user-profile floating-card">
+                <div className="User-profile-content">
+                    <label>{Auth.UserData.Firstname} {Auth.UserData.Lastname}</label>
+                    <label>Email: {Auth.UserData.Email}</label>
+                    <label>Username: {Auth.UserData.Username}</label>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Private/UserDashboardPage.js
+++ src/Pages/Private/UserDashboardPage.js
@@ -10,10 +10,9 @@ class UserDashboard extends Component {
 
     render() {
         return (
-            <div className="floating-card-wrapper floating-card">
-                <div>
-                    <h5 style={{ margin: "0 0 7px 0", color: "#707070"}}>You are currently logged in as:</h5>
-                    <UserProfile showChangeImageLink={true}/>
+            <div className="floating-card-wrapper">
+                <div className="floating-card">                    
+                    <UserProfile/>
                     <UserProfileSettings/>
                 </div>
             </div>
